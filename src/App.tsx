b03bfb6252430841668c818bb915803fa68d5386
Daniel Burt <dburt@qualtrics.com>
2023-07-26 12:05:29 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const date1 = new Date(2019, 8, 23);
+	const date2 = new Date();
+	const diff = Math.abs(date1.getTime() - date2.getTime());
+	const diffDays = Math.ceil(diff / (1000 * 3600 * 24));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 id="title">It has been {diffDays} days since Utah beat BYU</h2>
+			<img id="main-img" alt="" src="https://universe.byu.edu/wp-content/uploads/2022/08/21FTB-vs-Utah-01958-1920x1280.jpg"/>
     </div>
   );
 }
