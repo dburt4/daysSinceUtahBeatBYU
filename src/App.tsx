136b22b25f69cf49577ece26b6711606045d2bfd
dburt <dburt@qualtrics.com>
2024-08-12 14:54:23 -0600

    Fixing image and updating time diff logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const date1 = new Date('2019-08-29T22:00:00');
+  const date1 = new Date('2019-08-29T07:00:00');
 	const date2 = new Date();
+  date2.setHours(0, 30, 0, 0); // Set time to 00:30 AM 
 	const diff = Math.abs(date1.getTime() - date2.getTime());
 	const diffDays = Math.ceil(diff / (1000 * 3600 * 24));
   return (
     <div className="App">
       <h2 id="title">It has been {diffDays} days since Utah beat BYU</h2>
-			<img id="main-img" alt="" src="https://universe.byu.edu/wp-content/uploads/2022/08/21FTB-vs-Utah-01958-1920x1280.jpg"/>
+			<img id="main-img" alt="" src="https://dburtdevtest.yul1.qualtrics.com/ControlPanel/Graphic.php?IM=IM_vNuxA9VSAxTQ7dK"/>
     </div>
   );
 }
